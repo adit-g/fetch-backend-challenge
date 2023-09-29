041887a5aff45941d77f33f39792b201db9cac22
Adit G <dittykong5@gmail.com>
2023-09-29 17:34:27 -0500

    add comments to confusing sections

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 let pointsAvailable = 0; // stores the total points available to user
 
 // stores each transaction in chronological order in the following format:
-// [{ payer: "DANNON", points: 200, timestamp: "2022-11-02T14:00:00Z", balance: 100 },
-//  { payer: "UNILEVER", points: 300, timestamp: "2022-10-31T11:00:00Z", balance: 300 }]
+// [{ payer: "DANNON", points: 200, timestamp: "2022-10-31T14:00:00Z", balance: 100 },
+//  { payer: "UNILEVER", points: 300, timestamp: "2022-11-01T11:00:00Z", balance: 300 }]
 // note: the balance field stores the amount of unspent points from that transaction
+// note 2: this list will ALWAYS be sorted by timestamp because of the binary search 
+//         insertion algorithm below
 const transactions = [];
 
 // stores each payer's balances in the following format:
@@ -15,6 +17,7 @@ function getBalances() {
 }
 
 function addTransaction( transaction ) {
+    // add a new field for transaction balance and handle negative point values if necessary
     if (transaction.points < 0) {
         handleNegativeAddTransaction(transaction);
         transaction.balance = 0;
@@ -22,6 +25,8 @@ function addTransaction( transaction ) {
         transaction.balance = transaction.points;
     }
 
+    // find the index where the transaction should be stored in our array and handle any
+    // edge cases associated with the binary search algorithm
     let location = findLoc(transaction.timestamp);
     if (location == 0) {
         if (transactions[0] && transactions[0].timestamp < transaction.timestamp)
@@ -31,24 +36,17 @@ function addTransaction( transaction ) {
     }
     transactions.splice(location, 0, transaction);
 
-    pointsAvailable += transaction.points;
+    pointsAvailable += transaction.points; // update points value
 
+    // update payerBalances
     if (payerBalances[transaction.payer]) payerBalances[transaction.payer] += transaction.points;
     else payerBalances[transaction.payer] = transaction.points;
 }
 
-function findLoc(transactionDate, start, end) {
-    start = start || 0;
-    end = end || transactions.length;
-    const pivot = parseInt(start + (end - start) / 2, 10);
-    if (end - start <= 1 || transactions[pivot].timestamp === transactionDate) return pivot;
-    if (transactions[pivot].timestamp < transactionDate) {
-        return findLoc(transactionDate, pivot, end);
-    } else {
-        return findLoc(transactionDate, start, pivot);
-    }
-}
-
+// this function updates the balance values if a negative point value is sent through the
+// add endpoint. 
+// e.g. if { payer: "DANNON", points: -300 } comes in through the add endpoint, this function 
+// will take away a total of 300 points from previous DANNON transaction balances.
 function handleNegativeAddTransaction(transaction) {
     let points = -1 * transaction.points;
     for (let i = 0; i < transactions.length; i++) {
@@ -61,13 +59,35 @@ function handleNegativeAddTransaction(transaction) {
 
         if (points == 0) return;
     }
+
+    // if there are not enough points in previous transaction balances for this buyer,
+    // the function will throw an error and a 400 error code will be returned
     throw new Error('Cannot have a negative balance for a payer');
+}
+
+// a binary search insertion algorithm which returns the index right before where a
+// transaction should be stored in the transactions array
+// note: a return value of zero could mean either "store this transaction before the first
+//       element" or "store this transaction right after the first element". This edge
+//       case must be handled in the caller function
+function findLoc(transactionDate, start, end) {
+    start = start || 0;
+    end = end || transactions.length;
+    const pivot = parseInt(start + (end - start) / 2, 10);
+    if (end - start <= 1 || transactions[pivot].timestamp === transactionDate) return pivot;
+    if (transactions[pivot].timestamp < transactionDate) {
+        return findLoc(transactionDate, pivot, end);
+    } else {
+        return findLoc(transactionDate, start, pivot);
+    }
 }
 
 function spend( points ) {
     if (points > pointsAvailable) throw new Error('You do not have enough points for this transaction');
-    pointsAvailable -= points;
 
+    pointsAvailable -= points; // update points value
+
+    // update balances in transactions array
     const payersToSubstractFrom = {};
     for (let i = 0; i < transactions.length; i++) {
         let tr = transactions[i];
@@ -83,6 +103,7 @@ function spend( points ) {
 
     if (points != 0) throw new Error('You do not have enough points for this transaction');
 
+    // formulate response and update payerBalances
     const response = [];
     for (payer in payersToSubstractFrom) {
         payerBalances[payer] += payersToSubstractFrom[payer];
